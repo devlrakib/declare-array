1e148d665d5584bad0f48aef1a1ef868206f63f8
devlrakib <devrakib5@gmail.com>
2024-03-13 16:43:49 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendAge = [11, 24, 75, 105, 6];
+var picnicFee = [5000, 2000, 1000];
+var Naiyakas  = ["mahi",  "Apu", "Sabnoor"];
+var Vowel     = ['a', 'e', 'i', 'o', 'u'];
+console.log(Vowel.length);
+console.log(Naiyakas.length);
